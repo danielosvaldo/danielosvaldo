5e5c7064dab1b24b669c364d79739450f78d4e3d
danielosvaldo <manthysm@gmail.com>
2021-07-02 07:01:10 -0500

    second class, java coding

--- wallet/src/App.java
+++ wallet/src/App.java
@@ -1,9 +1,47 @@
 public class App {
 
-
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private double estatura;
+    private float peso;
+    private boolean estadoCivil;
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello Word!");
+        // tipos de datos y operadores
+       String res = "Hola";
+       int resultado = 15 + 32; 
+       int resultado3 = 125 - 32; 
+       double resultado2 = 25 / 3;
+       float resultado4 = 58 * 21;
+       int resultado5 = 21 % 9; 
+        // operadores unarios
+        resultado++; //resultado = resultado + 1
+        resultado--; //resultado = resultado - 1
+        resultado = -resultado;
+        resultado = +resultado;
+        // operadores logicos
+        boolean respuesta = true;
+        respuesta = 5 > 8;
+        respuesta = 5 < 8;
+        respuesta = 5 >= 8;
+        respuesta = 5 <= 8;
+        respuesta = 5 != 8;
+        respuesta = 5 == 8;
+        respuesta = 12 < 5 && 4 > 6;
+        respuesta = 14 < 25 || 1 > 5;
+        //operadores bit a bit
+        // &
+        // 
+        // |    
+        //Estructuras de control
+        if
 
+      
+
+
+
+    
     }
 }
